654b04728a6baeea0585dab83759c126037cef10
itz.fyp <38676958+itzfyp@users.noreply.github.com>
2020-04-27 11:40:48 +0530

    Basic Dom Updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// Sample : Creatge Header element with some text
+function CreateHader() {
+  // ELement
+  const headerEl = document.createElement("H1");
+
+  headerEl.id = "app-title";
+
+  // headerEl.textContent = "Dom manipulation";
+
+  // Text Node
+  const headerTxtEl = document.createTextNode("Dom Manipulation");
+
+  headerEl.appendChild(headerTxtEl);
+
+  const bodyEl = document.querySelector("body");
+
+  bodyEl.appendChild(headerEl);
+}
+
+CreateHader();
+
+function SetStylesForTitle() {
+  const headerEl = document.querySelector("#app-title");
+  headerEl.setAttribute("class", "header-txt");
+}
+
+SetStylesForTitle();
+
+function UpdateClassForTitle() {
+  const headerEl = document.querySelector("#app-title");
+  const className = headerEl.getAttribute("class");
+  if (className === "header-txt") {
+    headerEl.setAttribute("class", "header-txt active");
+  }
+}
+
+UpdateClassForTitle();
+
+// Sample : Create Script tag from JS
+function CreateScript() {
+  const scriptEl = document.createElement("script");
+  scriptEl.src = "./src/index.js";
+  scriptEl.async = true;
+  document.body.appendChild(scriptEl);
+}
+
+CreateScript();
